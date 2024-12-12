a6df325bf6f1938019fbff0885ccdd99a1e9bde2
BinhUIT <22520128@gm.uit.edu.vn>
2024-12-12 10:52:36 +0700

    Add edit book type for librarian

--- UI_Library/src/App.jsx
+++ UI_Library/src/App.jsx
@@ -14,6 +14,7 @@ import BookBorrowingSlipList from './Pages/BookBorrowingSlipList'
 import RegisterNewBookTitleForm from './Pages/RegisterNewBookTitle'
 import RegisterNewBookTypeForm from './Pages/RegisterNewBookType'
 import EditBookTitleForm from './Pages/EditBookTitle'
+import EditBookTypeForm from './Pages/EditBookType'
 function App(){
   return (
     <>
@@ -31,7 +32,8 @@ function App(){
               <Route path="/book_borrowing_slip_list" element={<BookBorrowingSlipList></BookBorrowingSlipList>}/> 
               <Route path="/register_book_title" element={<RegisterNewBookTitleForm></RegisterNewBookTitleForm>}/>
               <Route path="/register_book_type" element={<RegisterNewBookTypeForm></RegisterNewBookTypeForm>}/>
-              <Route path="/edit_book_title/:id" element={<EditBookTitleForm></EditBookTitleForm>}/>
+              <Route path="/edit_book_title/:id" element={<EditBookTitleForm></EditBookTitleForm>}/> 
+              <Route path="/edit_book_type/:id" element={<EditBookTypeForm></EditBookTypeForm>}/>
           </Routes>
      </div>
     </>
